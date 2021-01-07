5e345acf5d9c679e1a02d0525b958a6d9db05526
khansun <khan.mahmud@outlook.com>
2021-01-07 19:43:43 +0600

    firebase imports solved

---
+++ lab6/src/contexts/AuthContext.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect, useContext} from 'react';
+import {auth} from './../firebase';
+
+const AuthContext = React.createContext();
+
+const useAuth = ()=>{
+    return useContext(AuthContext);
+}
+
+const AuthProvider = (props) =>{
+    const [currentUser, setCurrentUser] = useState({});
+    const signUp = (email, password) =>{
+        return auth.createUserWithEmailPassword(email, password);
+    }
+   // useEffect(()=>{
+    //    const unsubscribe = auth.onAuthStateChanged((user)=>{
+          //  setCurrentUser(user);
+      //  });
+       // return unsubscribe;
+   // }, []);
+    return(
+        <AuthContext.Provider value = {{
+            currentUser, signUp
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
+
+export {AuthContext, AuthProvider, useAuth};
